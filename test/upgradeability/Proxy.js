348494c2fec77eefc64820a3aee2e9f8e69249a0
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2020-07-15 01:39:27 +0900

    add tests for fallback() of Proxy contract

--- test/upgradeability/Proxy.js
+++ test/upgradeability/Proxy.js
@@ -1,5 +1,5 @@
 const { accounts, contract, web3 } = require('@openzeppelin/test-environment');
-const { expectEvent }              = require('@openzeppelin/test-helpers');
+const { BN, expectEvent }          = require('@openzeppelin/test-helpers');
 const { expect }                   = require('chai');
 
 const Account = contract.fromArtifact('Account');
@@ -9,17 +9,36 @@ describe('Proxy', async () =>
 {
   const [ owner ] = accounts;
 
+  const zero  = new BN(0);
   const value = web3.utils.toWei('1', 'ether');
 
   beforeEach(async () =>
   {
-    this.impl  = await Account.new({ from: owner });
-    this.proxy = await Proxy.new(this.impl.address, { from: owner });
+    this.account      = await Account.new({ from: owner });
+    this.proxy        = await Proxy.new(this.account.address, { from: owner });
+    this.proxyAccount = await Account.at(this.proxy.address);
   });
 
   it('implementation', async () =>
   {
-    expect(await this.proxy.implementation()).to.be.equal(this.impl.address);
+    expect(await this.proxy.implementation()).to.be.equal(this.account.address);
+  });
+
+  it('fallback', async () =>
+  {
+    await this.proxyAccount.initialize(owner);
+
+    const data = this.account.contract.methods.execute(
+      this.proxy.address,
+      zero.toString(),
+      this.account.contract.methods.update(owner).encodeABI(),
+    ).encodeABI();
+
+    await this.proxy.sendTransaction({
+      from: owner,
+      data: data,
+    });
+    expect(await this.proxy.implementation()).to.be.equal(owner);
   });
 
   it('receive', async () =>
