e16c7ae0e8c37f4d7f7ba9685264758f0d025850
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2020-07-15 00:44:51 +0900

    update Account contract : implement update() function

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -5,7 +5,29 @@ import "./accessibility/Owned.sol";
 
 contract Account is Owned
 {
+  /*
+   * ref. EIP1967 https://eips.ethereum.org/EIPS/eip-1967
+   * obtained as bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1)
+   */
+  bytes32 internal constant IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+
   event Executed(address indexed dest, uint256 value, bytes data);
+  event Updated(address indexed impl);
+
+  modifier onlySelf
+  {
+    require(msg.sender == address(this), "must be self");
+    _;
+  }
+
+  function implementation() public view returns (address impl)
+  {
+    bytes32 slot = IMPLEMENTATION_SLOT;
+
+    assembly {
+      impl := sload(slot)
+    }
+  }
 
   function execute(address dest, uint256 value, bytes calldata data) public onlyOwner returns (bytes memory)
   {
@@ -20,5 +42,16 @@ contract Account is Owned
     emit Executed(dest, value, data);
 
     return result;
+  }
+
+  function update(address impl) public onlySelf
+  {
+    bytes32 slot = IMPLEMENTATION_SLOT;
+
+    assembly {
+      sstore(slot, impl)
+    }
+
+    emit Updated(impl);
   }
 }
--- test/Account.js
+++ test/Account.js
@@ -1,20 +1,36 @@
-const { accounts, contract } = require('@openzeppelin/test-environment');
-const { BN }                 = require('@openzeppelin/test-helpers');
-const { expect }             = require('chai');
+const { accounts, contract }                       = require('@openzeppelin/test-environment');
+const { BN, constants, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
+const { expect }                                   = require('chai');
+
+const { ZERO_ADDRESS } = constants;
 
 const Account = contract.fromArtifact('Account');
 const Tester  = contract.fromArtifact('Tester');
 
 describe('Account', async () =>
 {
-  const [ owner ] = accounts;
+  const [ owner, other ] = accounts;
 
+  const zero  = new BN(0);
   const value = new BN(1231006505);
 
   beforeEach(async () =>
   {
     this.account = await Account.new({ from: owner });
     this.tester  = await Tester.new({ from: owner });
+  });
+
+  it('update', async () =>
+  {
+    const data = this.account.contract.methods.update(owner).encodeABI();
+
+    expect(await this.account.implementation()).to.be.equal(ZERO_ADDRESS);
+
+    const receipt = await this.account.execute(this.account.address, zero, data, { from: owner });
+    expect(await this.account.implementation()).to.be.equal(owner);
+    expectEvent(receipt, 'Updated', { impl: owner });
+
+    await expectRevert(this.account.update(owner, { from: owner }), 'must be self');
   });
 
   it('execute', async () =>
