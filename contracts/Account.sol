4bba906e2bd6f395cb61de1a4812a7694418baf7
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2021-04-08 17:31:22 +0900

    renew with hardhat

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -1,66 +1,49 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.6.11;
+pragma solidity ^0.8.3;
 
-import "@openzeppelin/contracts/introspection/ERC165.sol";
 import "./TokenReceiver.sol";
 
-contract Account is ERC165, TokenReceiver
-{
-  address public owner;
+contract Account is TokenReceiver {
+    address public owner;
 
-  bool private _initialized = false;
+    event Executed(address indexed target, uint256 value, bytes data);
 
-  event Executed(address indexed dest, uint256 value, bytes data);
-  event OwnershipTransferred(address indexed prevOwner, address indexed newOwner);
+    event OwnershipTransferred(
+        address indexed prevOwner,
+        address indexed newOwner
+    );
 
-  modifier onlyOwner
-  {
-    require(msg.sender == owner, "must be owner");
-    _;
-  }
-
-  modifier onlyOwnerOrSelf
-  {
-    require(msg.sender == owner || msg.sender == address(this), "must be owner or self");
-    _;
-  }
-
-  function initialize(address initOwner) public
-  {
-    require(!_initialized, "already initialized");
-    _initialized = true;
-
-    owner = initOwner;
-    emit OwnershipTransferred(address(0), initOwner);
-
-    _registerInterfaces();
-  }
-
-  function execute(address dest, uint256 value, bytes memory data) public onlyOwner returns(bytes memory)
-  {
-    (bool success, bytes memory result) = dest.call{ value: value }(data);
-    if (!success) {
-      assembly {
-        returndatacopy(0, 0, returndatasize())
-        revert(0, returndatasize())
-      }
+    modifier onlyOwner {
+        require(msg.sender == owner, "must be owner");
+        _;
     }
 
-    emit Executed(dest, value, data);
+    function init(address initOwner) external {
+        require(owner == address(0), "already initialized");
+        emit OwnershipTransferred(address(0), initOwner);
+        owner = initOwner;
+    }
 
-    return result;
-  }
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "address must not be null");
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
 
-  function transferOwnership(address newOwner) public onlyOwnerOrSelf
-  {
-    require(newOwner != address(0), "address must not be null");
-    emit OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
+    function execute(
+        address target,
+        uint256 value,
+        bytes memory data
+    ) external onlyOwner returns (bytes memory) {
+        (bool success, bytes memory result) = target.call{value: value}(data);
+        if (!success) {
+            assembly {
+                returndatacopy(0, 0, returndatasize())
+                revert(0, returndatasize())
+            }
+        }
+        emit Executed(target, value, data);
 
-  function _registerInterfaces() private
-  {
-    // ERC1155TokenReceiver: bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)")) ^ bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))
-    _registerInterface(0x4e2312e0);
-  }
+        return result;
+    }
 }
