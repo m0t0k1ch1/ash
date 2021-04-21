0e43a3a8c605689962805618f014686dc92dd39d
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2021-04-21 20:50:34 +0900

    refactoring

---
+++ contracts/OwnerManager.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.3;
+
+contract OwnerManager {
+    address private _owner;
+
+    event OwnershipTransferred(
+        address indexed prevOwner,
+        address indexed newOwner
+    );
+
+    modifier onlyOwner {
+        require(_isOwner(msg.sender), "must be owner");
+        _;
+    }
+
+    modifier onlyOwnerOrSelf {
+        require(
+            _isOwner(msg.sender) || _isSelf(msg.sender),
+            "must be owner or self"
+        );
+        _;
+    }
+
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    function transferOwnership(address newOwner) external onlyOwnerOrSelf {
+        require(newOwner != address(0), "new owner cannot be the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+
+    function _setInitialOwner(address initOwner) internal {
+        require(_owner == address(0), "already initialized");
+        _owner = initOwner;
+        emit OwnershipTransferred(address(0), initOwner);
+    }
+
+    function _isOwner(address addr) internal view returns (bool) {
+        return addr == _owner;
+    }
+
+    function _isSelf(address addr) internal view returns (bool) {
+        return addr == address(this);
+    }
+}
--- test/Account.js
+++ test/Account.js
@@ -4,18 +4,12 @@ describe("Account contract", () => {
   let Account;
   let account;
 
-  let AshToken;
-  let ashToken;
-
   let owner;
   let other;
 
   beforeEach(async () => {
     Account = await ethers.getContractFactory("Account");
     account = await Account.deploy();
-
-    AshToken = await ethers.getContractFactory("AshToken");
-    ashToken = await AshToken.deploy();
 
     [owner, other] = await ethers.getSigners();
   });
@@ -33,27 +27,23 @@ describe("Account contract", () => {
     });
 
     it("onERC1155Received", async () => {
-      expect(
-        await account.onERC1155Received(
-          ethers.constants.AddressZero,
-          ethers.constants.AddressZero,
-          0,
-          0,
-          []
-        )
-      ).to.equal("0xf23a6e61"); // bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"))
+      await account.onERC1155Received(
+        ethers.constants.AddressZero,
+        ethers.constants.AddressZero,
+        0,
+        0,
+        []
+      );
     });
 
     it("onERC1155BatchReceived", async () => {
-      expect(
-        await account.onERC1155BatchReceived(
-          ethers.constants.AddressZero,
-          ethers.constants.AddressZero,
-          [],
-          [],
-          []
-        )
-      ).to.equal("0xbc197c81"); // bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))
+      await account.onERC1155BatchReceived(
+        ethers.constants.AddressZero,
+        ethers.constants.AddressZero,
+        [],
+        [],
+        []
+      );
     });
 
     it("init", async () => {
@@ -80,23 +70,18 @@ describe("Account contract", () => {
       expect(await account.owner()).to.equal(other.address);
     });
 
-    it("executeMetaTx", async () => {
+    it("execute", async () => {
       await account.init(owner.address);
-      await ashToken.transfer(
-        account.address,
-        await ashToken.balanceOf(owner.address)
-      );
 
-      const accountBalanceBefore = await ashToken.balanceOf(account.address);
-      const otherBalanceBefore = await ashToken.balanceOf(other.address);
-      const transferAmount = 1;
+      const nonceBefore = await account.nonce();
 
-      const to = ashToken.address;
+      const chainID = (await ethers.provider.getNetwork()).chainId;
+      const to = account.address;
       const value = 0;
-      const data = ashToken.interface.encodeFunctionData("transfer", [
+      const data = account.interface.encodeFunctionData("transferOwnership", [
         other.address,
-        transferAmount,
       ]);
+      const nonce = await account.nonce();
 
       const sigHashBytes = ethers.utils.arrayify(
         ethers.utils.keccak256(
@@ -111,31 +96,17 @@ describe("Account contract", () => {
               "bytes",
               "uint256",
             ],
-            [
-              0x19,
-              0x00,
-              account.address,
-              (await ethers.provider.getNetwork()).chainId,
-              to,
-              value,
-              data,
-              await account.nonce(),
-            ]
+            [0x19, 0x00, account.address, chainID, to, value, data, nonce]
           )
         )
       );
 
       await expect(
-        account.executeMetaTx(
-          to,
-          value,
-          data,
-          await other.signMessage(sigHashBytes)
-        )
+        account.execute(to, value, data, await other.signMessage(sigHashBytes))
       ).to.be.revertedWith("invalid signature");
 
       expect(
-        await account.executeMetaTx(
+        await account.execute(
           to,
           value,
           data,
@@ -144,16 +115,8 @@ describe("Account contract", () => {
       )
         .to.emit(account, "Executed")
         .withArgs(to, value, data);
-
-      const accountBalanceAfter = await ashToken.balanceOf(account.address);
-      const otherBalanceAfter = await ashToken.balanceOf(other.address);
-
-      expect(accountBalanceAfter.sub(accountBalanceBefore)).to.equal(
-        -transferAmount
-      );
-      expect(otherBalanceAfter.sub(otherBalanceBefore)).to.equal(
-        transferAmount
-      );
+      expect(await account.nonce()).to.equal(nonceBefore.add(1));
+      expect(await account.owner()).to.equal(other.address);
     });
   });
 });
